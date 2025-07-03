99c71daddfdfa0db4d49a1e379f71fc516b871bc
Arthur Leite <48776375+arthuraleite@users.noreply.github.com>
2025-07-03 05:17:23 -0300

    Fix Router comment formatting

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -2,4 +2,4 @@
 // app/Core/Router.php
 
 // Este arquivo pode ficar vazio, pois o roteamento já é feito no index.php
-// ou, se desejar centralizar o roteamento, mova a lógica de index.php pra cá futuramente.
+// Ou, se desejar centralizar o roteamento, mova a lógica de index.php pra cá futuramente.
